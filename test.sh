b7fb0c89cebc5dc731110a928023cf73a8ead5d8
openiolabs <andy@ionscope.com>
2017-10-25 15:23:57 +0200

    Doxygen update

--- test.sh
+++ test.sh
@@ -19,7 +19,6 @@ echo "${OUTPUT}"
 OUTPUT="$(git config --list)"
 echo "${OUTPUT}"
 
-
 cp -r /home/ubuntu/manual_builds/openio/src/scriptml/docs/c-api/* /home/ubuntu/github_repo/OpenIOLabs-Examples/documents/doxygen/c-api/.
 cp -r /home/ubuntu/manual_builds/openio/src/scriptml/docs/python-api/* /home/ubuntu/github_repo/OpenIOLabs-Examples/documents/doxygen/python-api/.
 
