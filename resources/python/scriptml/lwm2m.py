c2eb5d4bd078b6e5a10e5110d92f66b385b5051e
openiolabs <andy@ionscope.com>
2017-11-13 03:27:04 +0100

    Doxygen update

--- resources/python/scriptml/lwm2m.py
+++ resources/python/scriptml/lwm2m.py
@@ -3,7 +3,7 @@
 # 
 #  @brief LWM2M object and resource definitions
 # 
-#  Auto-generated 12 November 2017; do not edit
+#  Auto-generated 13 November 2017; do not edit
 #  
 #  Copyright (c) Open IO Labs Ltd 2017
 #  All Rights Reserved
