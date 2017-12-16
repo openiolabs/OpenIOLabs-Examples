8eaa39bc7548fe36c01b84426c19acba177ec2ac
openiolabs <andy@ionscope.com>
2017-12-16 03:26:45 +0100

    Doxygen update

--- resources/c/include/scriptml/lwm2m.h
+++ resources/c/include/scriptml/lwm2m.h
@@ -2,7 +2,7 @@
  * 
  *  @brief LWM2M object and resource definitions
  * 
- *  Auto-generated 15 December 2017; do not edit
+ *  Auto-generated 16 December 2017; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
--- resources/c/include/scriptml/mpu6050.h
+++ resources/c/include/scriptml/mpu6050.h
@@ -15,7 +15,7 @@
  *  
  *  
  *  
- *  Auto-generated 15 December 2017; do not edit
+ *  Auto-generated 16 December 2017; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
