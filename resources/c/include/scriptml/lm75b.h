0b8db89b31354018bf0ebf1121a68e75410e133e
openiolabs <andy@ionscope.com>
2017-12-30 03:06:35 +0100

    Doxygen update

--- resources/c/include/scriptml/lm75b.h
+++ resources/c/include/scriptml/lm75b.h
@@ -7,7 +7,7 @@
  *  
  *  Program the the `conf` register then read the `temp` register
  *  
- *  Auto-generated 29 December 2017; do not edit
+ *  Auto-generated 30 December 2017; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
--- resources/python/standard/math.py
+++ resources/python/standard/math.py
@@ -3,7 +3,7 @@
 # 
 #  @brief Math constants
 # 
-#  Auto-generated 29 December 2017; do not edit
+#  Auto-generated 30 December 2017; do not edit
 #  
 #  Copyright (c) Open IO Labs Ltd 2017
 #  All Rights Reserved
