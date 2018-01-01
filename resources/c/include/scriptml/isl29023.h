84eef67ad57833ff6793eaf16ef86bc9c99a0cb0
openiolabs <andy@ionscope.com>
2018-01-01 03:06:20 +0100

    Doxygen update

--- resources/c/include/scriptml/isl29023.h
+++ resources/c/include/scriptml/isl29023.h
@@ -8,7 +8,7 @@
  *  Program the two `command` registers and then read back from the `data` register.
  *  
  *  
- *  Auto-generated 31 December 2017; do not edit
+ *  Auto-generated 01 January 2018; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
--- resources/python/standard/stdlib_math.py
+++ resources/python/standard/stdlib_math.py
@@ -3,7 +3,7 @@
 # 
 #  @brief Standard library math functions
 # 
-#  Auto-generated 31 December 2017; do not edit
+#  Auto-generated 01 January 2018; do not edit
 #  
 #  Copyright (c) Open IO Labs Ltd 2017
 #  All Rights Reserved
