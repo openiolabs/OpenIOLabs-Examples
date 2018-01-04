5f29d5e020247fe2d45a741c88dcb41854a807ec
openiolabs <andy@ionscope.com>
2018-01-04 03:07:18 +0100

    Doxygen update

--- resources/python/scriptml/ad7091.py
+++ resources/python/scriptml/ad7091.py
@@ -5,7 +5,7 @@
 # 
 #  The ADI AD7091R quad channel ADC with 12 bits resolution, accessed via the I2C interface.
 #  
-#  Auto-generated 03 January 2018; do not edit
+#  Auto-generated 04 January 2018; do not edit
 #  
 #  Copyright (c) Open IO Labs Ltd 2017
 #  All Rights Reserved
