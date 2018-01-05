485e6faac9ebd5c03af0b493e96c4647a021ff7c
openiolabs <andy@ionscope.com>
2018-01-05 03:06:37 +0100

    Doxygen update

--- resources/c/include/scriptml/pca9534.h
+++ resources/c/include/scriptml/pca9534.h
@@ -9,7 +9,7 @@
  *  
  *  
  *  
- *  Auto-generated 04 January 2018; do not edit
+ *  Auto-generated 05 January 2018; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
