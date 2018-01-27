78fcf7f8cd21850cf683907489eb1a4c3b492535
openiolabs <andy@ionscope.com>
2018-01-27 03:06:33 +0100

    Doxygen update

--- resources/c/include/scriptml/mpl115a2.h
+++ resources/c/include/scriptml/mpl115a2.h
@@ -7,7 +7,7 @@
  *  
  *  Read out the constants `a0`, `b1`, `b2` and `c12`, then `convert` and finally read back `Padc` and `Tadc`
  *  
- *  Auto-generated 26 January 2018; do not edit
+ *  Auto-generated 27 January 2018; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
--- resources/python/standard/math.py
+++ resources/python/standard/math.py
@@ -3,7 +3,7 @@
 # 
 #  @brief Math constants
 # 
-#  Auto-generated 26 January 2018; do not edit
+#  Auto-generated 27 January 2018; do not edit
 #  
 #  Copyright (c) Open IO Labs Ltd 2017
 #  All Rights Reserved
