c63095dc131a7ab6c246cc9be30f0452eaa52211
openiolabs <andy@ionscope.com>
2018-02-10 03:06:21 +0100

    Doxygen update

--- resources/c/include/stdlib_math.h
+++ resources/c/include/stdlib_math.h
@@ -2,7 +2,7 @@
  * 
  *  @brief Standard library math functions
  * 
- *  Auto-generated 09 February 2018; do not edit
+ *  Auto-generated 10 February 2018; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
