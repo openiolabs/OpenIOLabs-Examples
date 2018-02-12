d2915edb86425d6550c0097f7901236fbdf1d77a
openiolabs <andy@ionscope.com>
2018-02-12 03:06:21 +0100

    Doxygen update

--- resources/python/standard/stdlib_math.py
+++ resources/python/standard/stdlib_math.py
@@ -3,7 +3,7 @@
 # 
 #  @brief Standard library math functions
 # 
-#  Auto-generated 11 February 2018; do not edit
+#  Auto-generated 12 February 2018; do not edit
 #  
 #  Copyright (c) Open IO Labs Ltd 2017
 #  All Rights Reserved
