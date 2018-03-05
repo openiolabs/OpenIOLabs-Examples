b7dfcd2038cc166a7c541517df9a77883ff543f1
openiolabs <andy@ionscope.com>
2018-03-05 03:06:22 +0100

    Doxygen update

--- resources/c/include/scriptml/mpu6050.h
+++ resources/c/include/scriptml/mpu6050.h
@@ -15,7 +15,7 @@
  *  
  *  
  *  
- *  Auto-generated 04 March 2018; do not edit
+ *  Auto-generated 05 March 2018; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
