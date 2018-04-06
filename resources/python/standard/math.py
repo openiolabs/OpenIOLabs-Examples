690d54fa1a6549264597ed0f7087f2f8607041a4
openiolabs <andy@ionscope.com>
2018-04-06 03:06:24 +0200

    Doxygen update

--- resources/python/standard/math.py
+++ resources/python/standard/math.py
@@ -3,7 +3,7 @@
 # 
 #  @brief Math constants
 # 
-#  Auto-generated 05 April 2018; do not edit
+#  Auto-generated 06 April 2018; do not edit
 #  
 #  Copyright (c) Open IO Labs Ltd 2017
 #  All Rights Reserved
