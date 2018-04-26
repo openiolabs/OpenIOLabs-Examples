56080663c64786efadc07d4fd471b3d052e12a64
openiolabs <andy@ionscope.com>
2018-04-26 03:06:24 +0200

    Doxygen update

--- resources/c/include/math.h
+++ resources/c/include/math.h
@@ -2,7 +2,7 @@
  * 
  *  @brief Math constants
  * 
- *  Auto-generated 25 April 2018; do not edit
+ *  Auto-generated 26 April 2018; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
