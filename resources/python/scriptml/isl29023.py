7ed95948de1eeecd1f334bdc2599ab1aaa3d716c
openiolabs <andy@ionscope.com>
2018-05-05 03:06:29 +0200

    Doxygen update

--- resources/python/scriptml/isl29023.py
+++ resources/python/scriptml/isl29023.py
@@ -9,7 +9,7 @@
 #  Program the two `command` registers and then read back from the `data` register.
 #  
 #  
-#  Auto-generated 04 May 2018; do not edit
+#  Auto-generated 05 May 2018; do not edit
 #  
 #  Copyright (c) Open IO Labs Ltd 2017
 #  All Rights Reserved
