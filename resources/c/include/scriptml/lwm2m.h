324cbd222dc6719ec3bd7b018d5caeb3772ddc87
openiolabs <andy@ionscope.com>
2018-05-11 03:06:34 +0200

    Doxygen update

--- resources/c/include/scriptml/lwm2m.h
+++ resources/c/include/scriptml/lwm2m.h
@@ -2,7 +2,7 @@
  * 
  *  @brief LWM2M object and resource definitions
  * 
- *  Auto-generated 10 May 2018; do not edit
+ *  Auto-generated 11 May 2018; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
--- resources/c/include/stdlib_math.h
+++ resources/c/include/stdlib_math.h
@@ -2,7 +2,7 @@
  * 
  *  @brief Standard library math functions
  * 
- *  Auto-generated 10 May 2018; do not edit
+ *  Auto-generated 11 May 2018; do not edit
  *  
  *  Copyright (c) Open IO Labs Ltd 2017
  *  All Rights Reserved
